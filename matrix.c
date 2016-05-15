a13de9997cd85f2a137c8a4b156488a56b150844
vixus0 <vixus0@gmail.com>
2016-05-15 22:13:00 +0000

    Map XY properly to LEDs

--- matrix.c
+++ matrix.c
@@ -19,7 +19,7 @@ ws2811_t leds =
             .gpionum = GPIO_PIN,
             .count = LED_COUNT,
             .invert = 0,
-            .brightness = 255,
+            .brightness = 80,
         },
         [1] =
         {
@@ -31,6 +31,20 @@ ws2811_t leds =
     },
 };
 
+int get_pixel_pos(uint8_t x, uint8_t y) {
+	int map[8][8] = {
+		{7 ,6 ,5 ,4 ,3 ,2 ,1 ,0 },
+		{8 ,9 ,10,11,12,13,14,15},
+		{23,22,21,20,19,18,17,16},
+		{24,25,26,27,28,29,30,31},
+		{39,38,37,36,35,34,33,32},
+		{40,41,42,43,44,45,46,47},
+		{55,54,53,52,51,50,49,48},
+		{56,57,58,59,60,61,62,63}
+	};
+
+	return map[x][y];
+}
 int matrix_init() {
   return ws2811_init(&leds);
 }
@@ -44,7 +58,7 @@ int matrix_render() {
 }
 
 void matrix_set(int x, int y, ws2811_led_t colour) {
-  leds.channel[0].leds[(y * WIDTH) + x] = colour;
+  leds.channel[0].leds[get_pixel_pos(x,y)] = colour;
 }
 
 void matrix_clear() {
